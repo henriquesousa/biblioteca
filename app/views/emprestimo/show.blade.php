40baaa39a195818c5bffe1230da863b0350f1892
Henrique <blecalt@gmail.com>
2014-11-07 06:29:15 -0200

    tttt

--- app/views/emprestimo/show.blade.php
+++ app/views/emprestimo/show.blade.php
@@ -7,8 +7,10 @@
 		<strong>Aluno :</strong> {{ $emprestimo->aluno->nome }}<br>
 		<strong>Livro :</strong> {{ $emprestimo->livro->nome}}<br>
 		<strong>Saida:</strong> {{ date('d/m/Y', strtotime($emprestimo->saida)) }}<br>
-		<strong>Previsão:</strong> {{ date('d/m/Y', strtotime($emprestimo->previsão)) }}<br> 
-		<strong>Entrega:</strong> {{ date('d/m/Y', strtotime($emprestimo->entrega_real)) }}<br>
+		<strong>Previsão:</strong> {{ date('d/m/Y', strtotime($emprestimo->previsao)) }}<br> 
+		<strong>Entrega:</strong> @if $emprestimo->entrega_real > $emprestimo->saidadate 
+		{{('d/m/Y', strtotime($emprestimo->entrega_real)) }}
+		@endif<br>
 		<strong>Multa:</strong> {{ $emprestimo->multa }}<br>
 		<strong>Funcionario:</strong> {{ $emprestimo->funcionario->nome}}<br>
 		
